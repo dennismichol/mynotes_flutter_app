b3f915ba7ceefe70809aef0654104ecb053e82e6
Dennis Michol Gabutin <dennisgabutin@gmail.com>
2022-03-06 14:37:51 +0800

    Refactoring
    Moved VerifyEmail view to new verify_email_view file

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -28,61 +28,64 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextField(
-          controller: _email,
-          enableSuggestions: false,
-          autocorrect: false,
-          keyboardType: TextInputType.emailAddress,
-          decoration: const InputDecoration(
-            hintText: 'Enter your email here',
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: 'Enter your email here',
+            ),
           ),
-        ),
-        TextField(
-          controller: _password,
-          obscureText: true,
-          enableSuggestions: false,
-          autocorrect: false,
-          decoration: const InputDecoration(
-            hintText: 'Enter your password here',
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(
+              hintText: 'Enter your password here',
+            ),
           ),
-        ),
-        TextButton(
-          onPressed: () async {
-            final email = _email.text;
-            final password = _password.text;
-            try {
-              final userCredential = await FirebaseAuth.instance
-                  .signInWithEmailAndPassword(email: email, password: password);
-              print('Hello');
-              print(userCredential);
-            } on FirebaseAuthException catch (e) {
-              switch (e.code) {
-                case "user-not-found":
-                  print('Invalid user');
-                  break;
-                case "wrong-password":
-                  print('Password is incorrect');
-                  break;
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+              try {
+                final userCredential = await FirebaseAuth.instance
+                    .signInWithEmailAndPassword(
+                        email: email, password: password);
+                print('Hello');
+                print(userCredential);
+              } on FirebaseAuthException catch (e) {
+                switch (e.code) {
+                  case "user-not-found":
+                    print('Invalid user');
+                    break;
+                  case "wrong-password":
+                    print('Password is incorrect');
+                    break;
+                }
               }
-            }
-          },
-          child: const Text('Login'),
-        ),
-        TextButton(
-          onPressed: () async {
-            final email = _email.text;
-            final password = _password.text;
-
-            final userCredential = await FirebaseAuth.instance
-                .createUserWithEmailAndPassword(
-                    email: email, password: password);
-            print(userCredential);
-          },
-          child: const Text('Signup'),
-        ),
-      ],
+            },
+            child: const Text('Login'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                '/register/',
+                (route) => false,
+              );
+            },
+            child: const Text('Signup'),
+          ),
+        ],
+      ),
     );
   }
 }
